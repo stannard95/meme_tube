0b96b353a57c604fe7ee49ca2f901c6a076d8463
stannard95 <14066588@brookes.ac.uk>
2017-10-01 20:27:34 +0100

    added comments and regarranged code

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -11,38 +11,63 @@ class PostsController < Sinatra::Base
 
   # creates new meme video and adds to database
 	post '/memes' do
-	
+		new_meme = Post.new
+		new_meme.title = params[:title]
+		new_meme.description = params[:description]
+		new_meme.url = params[:url]
+		new_meme.genre = param[:genre]
+		new_meme.save
+		redirect "/memes"
+
 	end
 
   # returns all the meme videos
 	get '/memes' do
+	@posts = Post.all
     erb :"posts/index"
 
 	end
 
   # directs to 'make meme video' page
 	get '/memes/new' do
+		erb :"memes/new"
 		
 	end
 
 
   # directs to select meme video page
-  get '/memes/:id' do
-  
-  end
+	get '/memes/:id' do
+		@post = Post.find(params[:id])
+		erb :"memes/show"
+	  
+	end
 
 	# directs to selected meme video page for editing
 	get "/memes/:id/edit" do
+		@post = Post.find(params[:id])
+  		erb :"memes/edit"
 		
 	end
 
   # Edits values of the meme video
 	put "/memes/:id" do
+	  id = params[:id]
+      meme = Post.find(params[:id])
+      meme.title = params[:title]
+	  meme.description = params[:description]
+	  meme.url = params[:url]
+	  meme.genre = param[:genre]
+      post.save
+
+      redirect "/memes/#{meme.id}"
 	
 	end
 
   # deletes meme video
   delete "/memes/:id" do
+  	id=params[:id]
+    Post.destroy(id)
+    redirect "/memes"
    
   end
 
